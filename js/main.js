e6a201ccd65967221b810d2791ebef8c48e9168d
Colin Geindre <colin.geindre@gmail.com>
2020-03-12 21:41:27 +0100

    ajout des notifs 1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-window.onload = function() {
+window.onload = function () {
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register('./sw.js')
-            .then(function () {
-                console.log('Service Worker Registered');
+            .then(function (sw) {
+                console.log('Service Worker ajouté :');
+                console.log(sw);
             });
+    }
+
+    Notification.requestPermission(function (status) {
+        console.log('Notification permission status:', status);
+    });
+}
+
+function displayNotification() {
+    if (Notification.permission == 'granted') {
+        navigator.serviceWorker.getRegistration().then(function (reg) {
+            reg.showNotification('Hello world!');
+            console.log('Notification sent !');
+        });
     }
 }
