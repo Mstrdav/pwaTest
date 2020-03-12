562783a909560fa4b5570f21779add6a4aa14171
Colin Geindre <colin.geindre@gmail.com>
2020-03-12 20:19:07 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./sw.js')
+            .then(function () {
+                console.log('Service Worker Registered');
+            });
+    }
+}
